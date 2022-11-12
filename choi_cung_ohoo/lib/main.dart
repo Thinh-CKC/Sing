7c25f74cfb7a996e9ad4edca8afebb018cde7aaa
tuananh <0306201210@caothang.edu.vn>
2022-11-12 19:33:18 +0700

    cap nhat import cho ket banscreen

--- choi_cung_ohoo/lib/main.dart
+++ choi_cung_ohoo/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:choi_cung_ohoo/screens/bat_dau_linh_vuc.dart';
 import 'package:choi_cung_ohoo/screens/che_do_du_muc.dart';
 import 'package:choi_cung_ohoo/screens/chu_de_cau_hoi.dart';
 import 'package:choi_cung_ohoo/screens/chu_de_linh_vuc.dart';
+import 'package:choi_cung_ohoo/screens/ket_ban.dart';
 
 import 'package:flutter/material.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Chơi cùng Ohoo',
       //theme: ThemeData(fontFamily: 'Verdana'),
       debugShowCheckedModeBanner: false,
-      home: ChuDeLinhVucScreen(),
+      home: KetBanScreen(),
       routes: {},
     );
   }
