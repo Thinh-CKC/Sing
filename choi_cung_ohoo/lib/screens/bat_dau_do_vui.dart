979d6da2cceb1db4bfe6664ecdc04b8c15d81d28
tuananh <0306201210@caothang.edu.vn>
2022-11-13 14:09:35 +0700

    chinh sua trang bat dau do vui

--- choi_cung_ohoo/lib/screens/bat_dau_do_vui.dart
+++ choi_cung_ohoo/lib/screens/bat_dau_do_vui.dart
@@ -16,7 +16,7 @@ class _BatDauDoVuiState extends State<BatDauDoVuiScreen> {
       height: MediaQuery.of(context).size.height,
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('images/BackgroundBlue.png'),
+          image: AssetImage('images/background-blue.png'),
           fit: BoxFit.cover,
         ),
       ),
@@ -57,7 +57,8 @@ class _BatDauDoVuiState extends State<BatDauDoVuiScreen> {
             height: 100,
             margin: EdgeInsets.fromLTRB(0, 107, 0, 0),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(60), color: Colors.white),
+                borderRadius: BorderRadius.circular(60), color: Colors.white,
+                image: DecorationImage(image: AssetImage('images/avatar-1.png'))),
             child: Container(
               padding: EdgeInsets.fromLTRB(20, 80, 20, 0),
               child: ElevatedButton(
