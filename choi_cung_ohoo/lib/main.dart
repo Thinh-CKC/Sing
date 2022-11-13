4d728fb5abb61349228471d09d2a0fc0afc41094
tuananh <0306201210@caothang.edu.vn>
2022-11-13 14:10:18 +0700

    cap nhat asset avatar

--- choi_cung_ohoo/lib/main.dart
+++ choi_cung_ohoo/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Chơi cùng Ohoo',
       //theme: ThemeData(fontFamily: 'Verdana'),
       debugShowCheckedModeBanner: false,
-      home: BatDauCaNhanScreen(),
+      home: KetBanScreen(),
       routes: {},
     );
   }
