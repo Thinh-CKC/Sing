93c0af5056f9e27268787e870b243b7ab278a9a1
PhuThinh <0306201291@caothang.edu.vn>
2022-11-25 10:04:59 +0700

    upload giao dien moi nguoi choi

---
+++ choi_cung_ohoo/lib/screens/doi_khang/moi_nguoi_choi.dart
@@ -0,0 +1,182 @@
+import 'package:choi_cung_ohoo/main.dart';
+import 'package:flutter/material.dart';
+
+class InviteDoiKhangScreen extends StatefulWidget {
+  const InviteDoiKhangScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InviteDoiKhangScreen> createState() => _InviteDoiKhangScreenState();
+}
+
+class _InviteDoiKhangScreenState extends State<InviteDoiKhangScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEEEEEE),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Container(
+              width: 350,
+              height: 44,
+              padding: EdgeInsets.only(left: 5, right: 10),
+              margin: EdgeInsets.fromLTRB(15, 28, 15, 0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    splashRadius: 1,
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Color(0xFF6560FF),
+                      shadows: <Shadow>[
+                        Shadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 0,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    'Chế Độ Đối Kháng',
+                    style: TextStyle(
+                      color: Color(0xFF6560FF),
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  IconButton(
+                    splashRadius: 1,
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.people,
+                      color: Color(0xFF6560FF),
+                      shadows: <Shadow>[
+                        Shadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 0,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 180,
+              margin: EdgeInsets.only(top: 85),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/backgrounds/map.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(15, 80, 15, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Image(
+                        image: AssetImage('images/avatars/avatar-3.png'),
+                        width: 80,
+                        height: 80,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text(
+                          'Bella',
+                          style: TextStyle(
+                            color: Color(0xFFFF0088),
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image(
+                        image: AssetImage('images/items/vs.png'),
+                        width: 80,
+                        height: 80,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image(
+                        image: AssetImage('images/avatars/avatar-9.png'),
+                        width: 80,
+                        height: 80,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text(
+                          'Angle',
+                          style: TextStyle(
+                            color: Color(0xFFFF0088),
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    offset: Offset(4, 4),
+                  )
+                ],
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF6560FF),
+                  onPrimary: Colors.white,
+                  minimumSize: Size(145, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Bắt đầu',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- choi_cung_ohoo/lib/screens/giao_dien_dau_tien.dart
+++ choi_cung_ohoo/lib/screens/giao_dien_dau_tien.dart
@@ -23,102 +23,114 @@ class _FirstScreenState extends State<FirstScreen> {
             fit: BoxFit.fill,
           ),
         ),
-        child: Column(children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 135, 0, 0),
-            child: Logo(),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-            decoration: BoxDecoration(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 135, 0, 0),
+              child: Logo(),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+              decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15),
                 boxShadow: [
                   BoxShadow(
                     color: Colors.black.withOpacity(0.2),
                     offset: Offset(5, 5),
                   )
-                ]),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                onPrimary: Colors.white,
-                primary: Color.fromARGB(255, 255, 0, 136),
-                minimumSize: Size(220, 65),
-                padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ],
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.white,
+                  primary: Color(0xFFFF0088),
+                  minimumSize: Size(220, 65),
+                  padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignInScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'ĐĂNG NHẬP',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignInScreen()),
-                );
-              },
-              child: Text(
-                'ĐĂNG NHẬP',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            decoration: BoxDecoration(
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15),
                 boxShadow: [
                   BoxShadow(
                     color: Colors.black.withOpacity(0.2),
                     offset: Offset(5, 5),
                   ),
-                ]),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                onPrimary: Colors.white,
-                primary: Colors.amber,
-                minimumSize: Size(220, 65),
-                padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ],
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.white,
+                  primary: Colors.amber,
+                  minimumSize: Size(220, 65),
+                  padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignUpScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'ĐĂNG KÝ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignUpScreen()),
-                );
-              },
-              child: Text(
-                'ĐĂNG KÝ',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 195),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              RichText(
-                text: TextSpan(
-                    text: 'Copyright',
-                    style: TextStyle(fontSize: 12),
-                    children: [
-                      WidgetSpan(
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 5, 1),
-                          child: Icon(
-                            Icons.copyright,
-                            color: Colors.white,
-                            size: 12,
+            Container(
+              margin: EdgeInsets.only(top: 195),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: 'Copyright',
+                      style: TextStyle(fontSize: 12),
+                      children: [
+                        WidgetSpan(
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 5, 1),
+                            child: Icon(
+                              Icons.copyright,
+                              color: Colors.white,
+                              size: 12,
+                            ),
                           ),
                         ),
-                      ),
-                      TextSpan(
-                        text: '2022 SING Studio',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ]),
+                        TextSpan(
+                          text: '2022 SING Studio',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ]),
-          ),
-        ]),
+            ),
+          ],
+        ),
       ),
     );
   }
